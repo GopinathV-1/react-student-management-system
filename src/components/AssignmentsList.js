fa1f508ebe7ee3b8e22e065431024c32d38067f4
Gopinath V <gopinath.v.16@mountblue.tech>
2021-07-03 15:58:06 +0530

    removed not used function

--- src/components/AssignmentsList.js
+++ src/components/AssignmentsList.js
@@ -4,16 +4,12 @@ import Assignment from './Assignment';
 
 const AssignmentsList = ({ assignments, setAssignments }) => {
 
-  const handleRemoveAssignment = (id) => {
-    setAssignments(assignments.filter((assignment) => assignment.id !== id));
-  };
-
   return (
     <React.Fragment>
       <div>
         {!_.isEmpty(assignments) ? (
           assignments.map((assignment) => (
-            <Assignment key={assignment.id} {...assignment} handleRemoveAssignment={handleRemoveAssignment} />
+            <Assignment key={assignment.id} {...assignment} />
           ))
         ) : (
           <h2 className="message content-section col-md-8">No assignments available. Please add some assignments.</h2>
